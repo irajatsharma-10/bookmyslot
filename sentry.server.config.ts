53a59d89ebfea7d402d036b25b9773499c5ed5ac
Rajat Sharma <rajat103982@gmail.com>
2026-08-11 20:30:00 +0530

    chore: final production cleanup and boilerplate removal

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,16 +1,7 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "",
-
-  // Configurable via env var. Default 0.2 (20%) for production.
-  // Set to 1.0 in staging/development for full visibility.
   tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE || "0.2"),
-
-  // Enable logs to be sent to Sentry
   enableLogs: true,
 });
